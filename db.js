ebf529fdd0d74c821d04405e3fd272c0c82ee31b
captaincoro11 <captaincoro444@gmail.com>
2024-05-26 14:06:56 +0530

    lelo

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({});
-const url = process.env.DATABASE_URL;
+const url = process.env.MONGODB_URI;
 
 exports.connectDatabase = ()=>{
     mongoose.connect(url).then((con)=>console.log(`DataBase Connected ${con.connection.host}`)).catch((error)=>console.log(error))
